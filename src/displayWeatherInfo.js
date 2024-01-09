e586d25e53b059a4a8d5ecc1dd9e1a776257f449
myinan <m.yasir.inan@gmail.com>
2024-01-09 16:29:21 +0300

    Display current day info on main-left container

--- src/displayWeatherInfo.js
+++ src/displayWeatherInfo.js
@@ -2,8 +2,15 @@ import "./displayWeatherInfo.css";
 import { events } from "./events";
 
 // Get references to top left container, inner container elements
-const conditionLeft = document.querySelector(".condition");
-const cityLeft = document.querySelector(".city");
+const conditionLeft = document.querySelector(
+  ".top-left-container > .condition",
+);
+const cityLeft = document.querySelector(".top-left-container > .city");
+const dateLeft = document.querySelector(".top-left-container > .date");
+const localTimeLeft = document.querySelector(
+  ".top-left-container > .local-time",
+);
+const tempLeft = document.querySelector(".top-left-container > .temperature");
 
 events.on("newCitySearched", (weatherData) => {
   loginfo(weatherData);
@@ -17,4 +24,7 @@ function loginfo(data) {
 function renderToLeft(data) {
   conditionLeft.textContent = data[0].condition;
   cityLeft.textContent = `${data[0].city}, ${data[0].country}`;
+  dateLeft.textContent = data[0].dateFormatted;
+  localTimeLeft.textContent = data[0].localTimeFormatted;
+  tempLeft.textContent = `${data[0].avgTempC} °C`;
 }
