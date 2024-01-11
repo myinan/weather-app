5ea12640bf263eb1d985a4ff5c88fd2a89fec8d2
myinan <m.yasir.inan@gmail.com>
2024-01-11 03:36:00 +0300

    Add footer elements

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import "./getWeatherInfo.js";
 import "./displayWeatherInfo.js";
 import { getCityInfo } from "./getWeatherInfo.js";
 import loadingSVG from "./assets/loading.svg";
+import githubSVG from "./assets/github-mark.svg";
 
 const loading = document.querySelector("#loading");
+const footerIconAnchor = document.querySelector(".footer-icon > a");
 
 loading.innerHTML = loadingSVG;
+footerIconAnchor.innerHTML = githubSVG;
+footerIconAnchor.setAttribute("href", "https://github.com/myinan");
+footerIconAnchor.setAttribute("title", "github.com/myinan");
 
 document.addEventListener("DOMContentLoaded", () => {
   getCityInfo("Bursa");
